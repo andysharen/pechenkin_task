04ede113b373bdf7215982a06fc9c6455214a51b
andysharen <andysharen1991@gmail.com>
2020-05-30 12:39:24 +0300

    Initial commit

---
+++ src/main/java/task3/Program.java
@@ -0,0 +1,15 @@
+package task3;
+
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        String str = "";
+        do {
+            System.out.println("Введите слово начинающиеся на букву А");
+            str = new Scanner(System.in).nextLine();
+        } while (!str.startsWith("А"));
+        System.out.println("Благодарю");
+    }
+}
